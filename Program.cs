9841eb3fc15721178d5f4ad5cadc1bc48c6dc387
Marius Riedl <marius.riedl99@gmail.com>
2022-09-14 01:32:37 +0200

    Add validation

--- Program.cs
+++ Program.cs
@@ -2,15 +2,25 @@
 
 while(true)
 {
-    var championId = Console.ReadLine();
+    var input = Console.ReadLine();
 
-    if(championId == string.Empty || championId == null)
+    if (input == string.Empty || input == null)
     {
+        continue;
+    }
+
+    var validationService = new ValidationService();
+    var validation = await validationService.Validate(input);
+
+    if(!validation)
+    {
+        Console.WriteLine($"{input} is not a valid input");
         continue;
     }
 
     var versionService = new VersionService();
     var version = await versionService.GetLatestVersion();
-    var championService = new ChampionService(championId, version);
+
+    var championService = new ChampionService(input, version);
     championService.PrintChampionData();
 }
---
+++ ValidationService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace champion_terminal
+{
+    internal class ValidationService
+    {
+        private static string ChampionsUrl => "https://ddragon.leagueoflegends.com/cdn/12.17.1/data/en_US/champion.json";
+
+        private static readonly HttpClient Client = new();
+
+        public async Task<bool> Validate(string input)
+        {
+            var champions = await GetAllChampions();
+            var match = champions.FirstOrDefault(str => str.Contains(input));
+
+            if (match == null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private static async Task<List<string>> GetAllChampions()
+        {
+            var response = await Client.GetAsync(ChampionsUrl);
+            var responseBody = await response.Content.ReadAsStringAsync();
+            var championsData = JObject.Parse(responseBody);
+            var championsToken = championsData?.SelectToken("data");
+            var champions = new List<string>();
+
+            if (championsToken == null)
+            {
+                return champions;
+            }
+
+            foreach (var champion in championsToken)
+            {
+                champions.Add(champion?.First()?.SelectToken("id")?.ToString() ?? string.Empty);
+            }
+
+            return champions;
+        }
+    }
+}
