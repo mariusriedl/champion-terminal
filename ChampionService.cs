2a75989a33a96c42c0f685a93f56ed2d3a3b026d
Marius Riedl <marius.riedl99@gmail.com>
2022-09-13 23:03:41 +0200

    Remove todo

--- ChampionService.cs
+++ ChampionService.cs
@@ -12,11 +12,10 @@ namespace champion_terminal
 
         private string ChampionUrl { get; set; }
 
-        // TODO use latest version from version service
         private static string ChampionBaseUrl => "https://ddragon.leagueoflegends.com/cdn/version/data/en_US/champion/id.json";
 
         private static readonly HttpClient Client = new HttpClient();
-
+        
         public ChampionService(string championId, string version)
         {
             ChampionId = championId;
