8816af596bd81328bd685c13d49f18a5dfebb4c4
Marius Riedl <marius.riedl99@gmail.com>
2022-09-14 01:52:41 +0200

    Move riot api logic to service

---
+++ RiotApiService.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json.Linq;
+
+
+namespace champion_terminal
+{
+    internal static class RiotApiService
+    {
+        private static string ChampionsUrl => "https://ddragon.leagueoflegends.com/cdn/12.17.1/data/en_US/champion.json";
+
+        private static readonly HttpClient Client = new();
+
+        public static async Task<List<string>> GetAllChampions()
+        {
+            var response = await Client.GetAsync(ChampionsUrl);
+            var responseBody = await response.Content.ReadAsStringAsync();
+            var championsData = JObject.Parse(responseBody);
+            var championsToken = championsData?.SelectToken("data");
+            var champions = new List<string>();
+
+            if (championsToken == null)
+            {
+                return champions;
+            }
+
+            foreach (var champion in championsToken)
+            {
+                champions.Add(champion?.First()?.SelectToken("id")?.ToString() ?? string.Empty);
+            }
+
+            return champions;
+        }
+    }
+}
--- ValidationService.cs
+++ ValidationService.cs
@@ -1,18 +1,12 @@
-﻿using Newtonsoft.Json.Linq;
-
-namespace champion_terminal
+﻿namespace champion_terminal
 {
     internal class ValidationService
     {
-        private static string ChampionsUrl => "https://ddragon.leagueoflegends.com/cdn/12.17.1/data/en_US/champion.json";
-
-        private static readonly HttpClient Client = new();
-
         public ValidationService() { }
 
         public async Task<bool> Validate(string input)
         {
-            var champions = await GetAllChampions();
+            var champions = await RiotApiService.GetAllChampions();
             var match = champions.FirstOrDefault(str => str.Contains(input));
 
             if (match == null)
@@ -21,27 +15,6 @@ namespace champion_terminal
             }
 
             return true;
-        }
-
-        private static async Task<List<string>> GetAllChampions()
-        {
-            var response = await Client.GetAsync(ChampionsUrl);
-            var responseBody = await response.Content.ReadAsStringAsync();
-            var championsData = JObject.Parse(responseBody);
-            var championsToken = championsData?.SelectToken("data");
-            var champions = new List<string>();
-
-            if (championsToken == null)
-            {
-                return champions;
-            }
-
-            foreach (var champion in championsToken)
-            {
-                champions.Add(champion?.First()?.SelectToken("id")?.ToString() ?? string.Empty);
-            }
-
-            return champions;
         }
     }
 }
